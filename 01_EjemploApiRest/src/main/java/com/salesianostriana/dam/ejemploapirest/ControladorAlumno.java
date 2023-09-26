811bf0573c0924bec78cda346e8bf85c30e994c4
Fran Ruiz <fjavierrule@gmail.com>
2023-09-26 13:35:14 +0200

    guardado

--- 01_EjemploApiRest/src/main/java/com/salesianostriana/dam/ejemploapirest/ControladorAlumno.java
+++ 01_EjemploApiRest/src/main/java/com/salesianostriana/dam/ejemploapirest/ControladorAlumno.java
@@ -1,10 +1,11 @@
 package com.salesianostriana.dam.ejemploapirest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,4 +18,29 @@ public class ControladorAlumno {
         return repositorioAlumno.findAll();
     }
 
+    @GetMapping("/alumno/{id}")
+    public Alumno getById(@PathVariable int id){
+        return repositorioAlumno.findById(id)
+                .orElse(null);
+    }
+
+    @PostMapping("/alumno/")
+    public Alumno create(@RequestBody Alumno alumno){
+        return repositorioAlumno.save(alumno);
+    }
+
+    @PutMapping("/alumno/{id}")
+    public Alumno edit(@PathVariable int i, @RequestBody Alumno alumno){
+        return repositorioAlumno.edit(alumno);
+    }
+
+    @DeleteMapping("/alumno/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id){
+        Optional<Alumno> a = repositorioAlumno.findById(id);
+
+        if(a.isPresent())
+            repositorioAlumno.delete(a.get());
+
+        return ResponseEntity.noContent().build();
+    }
 }
