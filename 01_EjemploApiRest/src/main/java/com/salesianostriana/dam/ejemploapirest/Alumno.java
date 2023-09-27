accd60af785f6b1ba0b3e5b8f21c585dda6c4228
Fran Ruiz <fjavierrule@gmail.com>
2023-09-27 10:19:34 +0200

    ResponseEntity, Get, Put...

--- 01_EjemploApiRest/src/main/java/com/salesianostriana/dam/ejemploapirest/Alumno.java
+++ 01_EjemploApiRest/src/main/java/com/salesianostriana/dam/ejemploapirest/Alumno.java
@@ -1,10 +1,12 @@
 package com.salesianostriana.dam.ejemploapirest;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @Builder
@@ -13,7 +15,10 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Alumno {
 
+    @Id
+    @GeneratedValue
     private long id;
+    
     private String nombre, apellidos, email;
     private int edad;
 
