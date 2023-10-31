9778b197240db9382e06b7a05442466f79c31a1d
Fran Ruiz <fjavierrule@gmail.com>
2023-10-31 13:34:29 +0100

    conectar base de datos

---
+++ BaseDeDatos/src/test/java/com/example/BaseDeDatos/BaseDeDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaseDeDatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDeDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
